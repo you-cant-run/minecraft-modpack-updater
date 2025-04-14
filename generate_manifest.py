0b14c186df5a5f019a7a41d6819def9bb2cf14b7
ycr <modysola2@gmail.com>
2025-04-14 22:46:16 +0200

    Update generate_manifest.py

--- generate_manifest.py
+++ generate_manifest.py
@@ -25,12 +25,13 @@ def generate_manifest():
     # Loop through the mods folder and add them to the manifest
     for filename in os.listdir(MODS_DIR):
         if filename.endswith('.jar'):
-            mod_path = os.path.join(MODS_DIR, filename)
+            filepath = os.path.join(MODS_DIR, filename)
+            sha256_hash = compute_sha256(filepath)  # Compute SHA256 hash of the mod file
             mod = {
                 "name": filename,
                 "version": "1.0.0",  # You can add version info here if applicable
                 "file": f"{MODS_DIR}/{filename}",
-                "sha256": compute_sha256(mod_path)  # Add SHA256 hash
+                "sha256": sha256_hash  # Add the SHA256 hash here
             }
             manifest["modpack"]["mods"].append(mod)
 
